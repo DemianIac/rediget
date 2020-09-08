f1e91e9c9f2aed6907c6d1426b716b80997e6725
Demian Iacono <demian.iacono@etermax.com>
2020-09-08 18:53:04 -0300

    Refactor to re-use code and reduce the ammount responsability of activity

--- app/src/main/java/com/diacono/rediget/reader/presentation/PostListActivity.kt
+++ app/src/main/java/com/diacono/rediget/reader/presentation/PostListActivity.kt
@@ -20,8 +20,6 @@ import org.koin.androidx.viewmodel.scope.stateViewModel
 class PostListActivity : AppCompatActivity() {
 
     private val viewModel: PostListViewModel by lifecycleScope.stateViewModel(this)
-    private var mLayoutManager: LinearLayoutManager = LinearLayoutManager(this)
-    private var loading = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,70 +29,28 @@ class PostListActivity : AppCompatActivity() {
         setupToolbar()
     }
 
+    private fun observeProperty() {
+        viewModel.selectedPost.observe(this, Observer {
+            if (!isInTwoPaneMode())
+                moveToSingleDetail()
+        })
+    }
+
     private fun setupView() {
         if (isInTwoPaneMode()) {
             supportFragmentManager.inTransaction {
                 replace(R.id.post_detail_container, PostDetailFragment.newInstance())
             }
-        } else {
-            supportFragmentManager.inTransaction {
-                replace(R.id.activity_container, PostListFragment.newInstance()).addToBackStack(
-                    POST_LIST
-                )
-            }
         }
-    }
-
-    private fun observeProperty() {
-        viewModel.postList.observe(this, Observer {
-            loading = true
-            vSwipeToRefresh.isRefreshing = false
-            setupRecyclerView(findViewById(R.id.item_list), it)
-        })
+        supportFragmentManager.inTransaction {
+            replace(R.id.activity_container, PostListFragment.newInstance())
+        }
     }
 
     private fun setupToolbar() {
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
         toolbar.title = title
-    }
-
-    private fun setupRecyclerView(
-        recyclerView: RecyclerView,
-        posts: List<Post>
-    ) {
-        //Todo PagedList implementation would be better here
-        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                if (dy > 0) {
-                    if (loading) {
-                        if (needToLoadMorePosts()) {
-                            loading = false
-                            viewModel.onNeedToLoadMorePosts()
-                        }
-                    }
-                }
-            }
-        })
-        recyclerView.layoutManager = mLayoutManager
-        recyclerView.adapter = PostRecyclerViewAdapter(
-            posts,
-            itemClickListener()
-        )
-        vSwipeToRefresh.setOnRefreshListener(
-            SwipeRefreshLayout.OnRefreshListener { viewModel.refreshPosts() }
-        )
-    }
-
-    private fun needToLoadMorePosts(): Boolean {
-        with(mLayoutManager) { return childCount + findFirstVisibleItemPosition() >= itemCount }
-    }
-
-    private fun itemClickListener() = View.OnClickListener { v ->
-        val item = v.tag as Post
-        viewModel.onSelectedPost(item)
-        if (!isInTwoPaneMode())
-            moveToSingleDetail()
     }
 
     private fun moveToSingleDetail() {
--- app/src/main/java/com/diacono/rediget/reader/presentation/PostListFragment.kt
+++ app/src/main/java/com/diacono/rediget/reader/presentation/PostListFragment.kt
@@ -1,17 +1,68 @@
 package com.diacono.rediget.reader.presentation
 
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout.OnRefreshListener
+
+import android.view.View
 import com.diacono.rediget.R
 import com.diacono.rediget.commons.BaseFragment
 import kotlinx.android.synthetic.main.fragment_post_list.*
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.diacono.rediget.reader.domain.model.Post
 
 
 class PostListFragment : BaseFragment() {
 
     val viewModel: PostListViewModel by sharedViewModel()
+    private var mLayoutManager: LinearLayoutManager = LinearLayoutManager(context)
+    private var loading = true
 
     override fun layout() = R.layout.fragment_post_list
+
+    override fun observeProperty() {
+        viewModel.postList.observe(this, Observer {
+            loading = true
+            vSwipeToRefresh.isRefreshing = false
+            setupRecyclerView(it)
+        })
+    }
+
+    private fun setupRecyclerView(posts: List<Post>) {
+        //Todo PagedList implementation would be better here
+        vPostListRecycler.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                loadMoreIfNeeded(dy)
+            }
+        })
+        vPostListRecycler.layoutManager = mLayoutManager
+        vPostListRecycler.adapter = PostRecyclerViewAdapter(
+            posts,
+            itemClickListener()
+        )
+        vSwipeToRefresh.setOnRefreshListener(
+            SwipeRefreshLayout.OnRefreshListener { viewModel.refreshPosts() }
+        )
+    }
+
+    private fun loadMoreIfNeeded(dy: Int) {
+        if (needsToLoadMorePosts(dy)) {
+            loading = false
+            viewModel.onNeedToLoadMorePosts()
+        }
+    }
+
+    private fun needsToLoadMorePosts(deltaY: Int) = deltaY > 0 && loading && nearToLastVisible()
+
+    private fun nearToLastVisible(): Boolean {
+        with(mLayoutManager) { return childCount + findFirstVisibleItemPosition() >= itemCount }
+    }
+
+    private fun itemClickListener() = View.OnClickListener { v ->
+        val item = v.tag as Post
+        viewModel.onSelectedPost(item)
+    }
 
     companion object {
         fun newInstance() = PostListFragment()
